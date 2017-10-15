c8c1caebc4b26e25cdddc7854e343065361b48e6
liujun <jliu@varitrip.com>
2017-10-15 11:07:35 +0800

    complete a simple chat server

---
+++ chat/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+
+	"github.com/gorilla/handlers"
+)
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	tpl      *template.Template
+}
+
+func (h *templateHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	h.once.Do(func() {
+		h.tpl = template.Must(template.ParseFiles(filepath.Join("templates", h.filename)))
+	})
+	h.tpl.Execute(w, req)
+}
+
+func main() {
+	var addr = flag.String("addr", ":8888", "The addr of the application")
+	flag.Parse()
+	var r = newRoom()
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+	go r.run()
+	log.Println("Starting web server on ", *addr)
+	if err := http.ListenAndServe(*addr, handlers.LoggingHandler(os.Stdout, http.DefaultServeMux)); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
---
+++ chat/room.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+type room struct {
+	forward chan []byte
+	join    chan *client
+	leave   chan *client
+	clients map[*client]bool
+}
+
+func newRoom() *room {
+	return &room{
+		forward: make(chan []byte),
+		join:    make(chan *client),
+		leave:   make(chan *client),
+		clients: make(map[*client]bool),
+	}
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case c := <-r.join:
+			r.clients[c] = true
+		case c := <-r.leave:
+			delete(r.clients, c)
+			close(c.send)
+		case msg := <-r.forward:
+			for c := range r.clients {
+				c.send <- msg
+			}
+		}
+	}
+}
+
+var upgrader = &websocket.Upgrader{
+	ReadBufferSize:  socketBufferSize,
+	WriteBufferSize: socketBufferSize,
+}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServeHTTP: ", err)
+		return
+	}
+	client := &client{
+		socket: socket,
+		send:   make(chan []byte, messageBufferSize),
+		room:   r,
+	}
+	r.join <- client
+	defer func() { r.leave <- client }()
+	go client.Write()
+	client.Read()
+}
